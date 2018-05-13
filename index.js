eaad9ba734e2c75f3e7ae61d6db32e601aeb81b4
Daniel Ruf <daniel@daniel-ruf.de>
2018-05-13 23:05:41 +0200

    chore: describe needed logic

---
+++ index.js
@@ -0,0 +1,12 @@
+const pkg = require('./package.json')
+
+console.log(pkg.name + ' ' + pkg.version)
+
+// @todo: logic
+// page.goto(SERVICE_ENDPOINT_URL)
+// new promise / async / await
+// page.type(selector, string)
+// page.click(selector)
+// await page.emulateMedia('screen');
+// await page.pdf({path: 'file.pdf'});
+// promise resolve / callback
