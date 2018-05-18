a2d3f3251cad025578ba2ec91890e8b6bf2e277a
Daniel Ruf <daniel@daniel-ruf.de>
2018-05-18 23:29:28 +0200

    feat: implement working SSLLabs task

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ console.log(pkg.name + ' ' + pkg.version)
 
 
 const puppeteer = require('puppeteer')
+const chalk = require('chalk')
 const devices = require('puppeteer/DeviceDescriptors')
-const url = process.argv[2];
+const url = process.argv[2]
+
+if(!process.argv[2]) {
+  console.log(chalk.red('No website was provided.'))
+  process.exit(1)
+  return
+}
 
 (async() => {
-  const browser = await puppeteer.launch({headless: false})
+  const browser = await puppeteer.launch({headless: true})
   const page = await browser.newPage()
   await page._client.send('Emulation.clearDeviceMetricsOverride')
   let warning
@@ -38,7 +45,10 @@ const url = process.argv[2];
     if(intervalID)clearInterval(intervalID)
     const links = await page.evaluate(() => [...document.querySelectorAll('#multiTable a')].map(link => link.href))
     const linksLength = links.length
-    links.forEach((link)=>console.log(link))
+    for(let i = 0; i < linksLength; i++){
+      await page.goto(links[i])
+      await page.pdf({path: './pdf-'+i+'.pdf', format: 'A4', printBackground: true})
+    }
     await browser.close()
   }
   // await page.goto('https://www.ssllabs.com/ssltest/');
