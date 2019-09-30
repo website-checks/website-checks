c44e7d443c24e4d5c403f604625a5069ebbddf1c
Daniel Ruf <daniel@daniel-ruf.de>
2019-09-30 23:04:17 +0200

    Wait for the results

--- modules/checks/httpobservatory.js
+++ modules/checks/httpobservatory.js
@@ -6,8 +6,8 @@ module.exports = async () => {
     const name = 'HTTP Observatory'
     async function tryBlock(page) {
       await page._client.send('Emulation.clearDeviceMetricsOverride')
-      await page.goto('https://observatory.mozilla.org/analyze/' + url + '&third-party=false', { waitUntil: 'networkidle0' })
-      await page.waitForFunction('!document.querySelector("#scan-progress-bar")', { timeout: 240000 })
+      await page.goto('https://observatory.mozilla.org/analyze/' + url + '?third-party=false', { waitUntil: 'networkidle0' })
+      await page.waitForSelector('#http-results', { timeout: 240000, visible: true })
       await page.emulateMedia('screen')
       await page.pdf({ path: path.resolve(output_path, './httpobservatory.pdf'), scale: 0.75, format: 'A4', printBackground: true })
     }
