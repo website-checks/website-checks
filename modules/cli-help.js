3729cf004c12de5c13cb545f4bcb8351b1bed9cb
Daniel Ruf <mac1@daniel-ruf.de>
2020-02-13 19:38:43 +0100

    Correctly resolve path

--- modules/cli-help.js
+++ modules/cli-help.js
@@ -1,11 +1,13 @@
 const fs = require('fs')
+const path = require('path')
 
 module.exports = () => {
   if (options_keys.includes('--help')) {
     console.log(`
     Available options for website-checks:
     `)
-    const checks = fs.readdirSync('./modules/checks')
+    const dirPath = path.resolve(__dirname, 'checks')
+    const checks = fs.readdirSync(dirPath)
     checks.forEach(check => {
       const help = require('./checks/' + check).help
       if (typeof help !== 'undefined') {
