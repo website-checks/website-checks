560258e85e93510225681f7dda304bb6be295012
Isaak Eriksson <isaak.eriksson@protonmail.com>
2021-08-27 21:48:28 +0300

    Fix yellowlab.tools scraper

--- modules/checks/yellowlab.js
+++ modules/checks/yellowlab.js
@@ -11,10 +11,21 @@ module.exports = async () => {
       await page.type(".url", url)
       await page.click(".launchBtn")
       await page.waitFor(".globalGrade", { timeout: 240000 })
-      await page.pdf({ path: path.resolve(output_path, './yellow-lab-1.pdf'), format: 'A4', printBackground: true })
-      await page.click("div.menuItem:nth-child(4)")
-      await page.waitFor(3000);
-      await page.pdf({ path: path.resolve(output_path, './yellow-lab-2.pdf'), format: 'A4', printBackground: true })
+      await page.pdf({ path: path.resolve(output_path, './yellow-lab-overview.pdf'), format: 'A4', printBackground: true })
+      const categoryLength = await page.$$eval('div[ng-repeat="categoryKey in categoriesOrder"]', divs => divs.length)
+      for (let i_cat = 1; i_cat <= categoryLength; i_cat++) {
+        const categoryName = await page.$eval(`div[ng-repeat="categoryKey in categoriesOrder"]:nth-of-type(${i_cat})`, div => div.innerText.split("\t")[1])
+        const linksLength = await page.$$eval(`div[ng-repeat="categoryKey in categoriesOrder"]:nth-of-type(${i_cat}) a`, as => as.length)
+        for (let j_link = 1; j_link <= linksLength; j_link++) {
+          const className = `div[ng-repeat="categoryKey in categoriesOrder"]:nth-of-type(${i_cat}) a:nth-of-type(${j_link})`
+          const linkName = await page.$eval(className, div => div.innerText.split("\t")[1].replace("/", " or ").replace(" ", "-"))
+          await page.click(className)
+          await page.waitFor(".ruleTable", { timeout: 240000 })
+          await page.pdf({ path: path.resolve(output_path, `./yellow-lab-${categoryName}-${linkName}.pdf`), format: 'A4', printBackground: true })
+          await page.goBack()
+          await page.waitFor(".globalGrade", { timeout: 240000 })
+        }
+      }
     }
     await checkFunction('Yellow Lab Tools', tryBlock)
   }
