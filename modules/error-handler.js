165ba2ecdc5bb41083a71f03590968f85fd7b6e3
Isaak Eriksson <isaak.eriksson@protonmail.com>
2021-10-05 08:43:39 +0200

    Export PDF on failure (fixes #42)

--- modules/error-handler.js
+++ modules/error-handler.js
@@ -1,13 +1,21 @@
-'use strict';
+"use strict";
 
-const { red } = require('kleur')
-const teardown = require('./teardown')
+const { red } = require("kleur");
+const path = require("path");
+const teardown = require("./teardown");
 
 module.exports = async (params) => {
-  let [ err, page, name ] = params
-  console.log(red('[error] ' + name), red(err))
-  errors++
-  await page.close()
-  open_pages--
-  await teardown()
-}
+  const [err, page, name] = params;
+  const errorpath = path.resolve(output_path, `./${name}-failed.pdf`);
+  console.log(red("[error] " + name), red(err));
+  errors++;
+  await page.pdf({
+    path: errorpath,
+    format: "A4",
+    printBackground: true,
+  });
+  console.log(red(`[error] ${name}: Failed document written to ${errorpath}`));
+  await page.close();
+  open_pages--;
+  await teardown();
+};
