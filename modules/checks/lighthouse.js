20017a82731ffa77d19adda2c779fc19ed60a24d
Isaak Eriksson <4984415+myisaak@users.noreply.github.com>
2024-05-16 05:25:16 +0000

    Fix Lightouse check

--- modules/checks/lighthouse.js
+++ modules/checks/lighthouse.js
@@ -4,17 +4,18 @@ const path = require('path')
 const retry = require('../retry')
 const checkFunction = require('../check-function')
 
+// TODO: ensure UA's language is set to English
 module.exports = async () => {
   if (no_cli_flags || options_keys.includes('--lighthouse')) {
     const name = 'Lighthouse'
     async function tryBlock(page) {
       await page._client.send('Emulation.clearDeviceMetricsOverride')
-      await retry(() => page.goto('https://lighthouse-ci.appspot.com/try'), 1000)
-      await page.type('#url', url)
-      await page.click('.url-section .search-arrow')
-      await page.waitForSelector('body.done', { timeout: 60000 })
-      const link = await page.evaluate(() => document.querySelector('#reportLink').href)
-      await page.goto(link)
+      await retry(() => page.goto('https://pagespeed.web.dev/'), 1000)
+      await page.type('[name="url"]', url)
+      const button = await page.waitForXPath(`//span[contains(text(), 'Analy')]`);
+      await button.click();
+      await page.waitForXPath(`//div[contains(text(), 'Report from')]`, { timeout: 30000 });
+      await page.waitForTimeout(30000); // TODO: find suitable selector
       await page.pdf({ path: path.resolve(output_path, './lighthouse.pdf'), format: 'A4', printBackground: true })
     }
     await checkFunction(name, tryBlock)
